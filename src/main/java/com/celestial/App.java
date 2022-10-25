4213dc26340fcc7203e8678f187700fa76095f39
yawenGit <arwen.zhang@outlook.com>
2022-10-25 23:07:53 +0100

    try to recreate

--- src/main/java/com/celestial/App.java
+++ src/main/java/com/celestial/App.java
@@ -12,18 +12,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        MsgElementReader mer = new MsgElementReader();
-        MsgLineReader mlr = new MsgLineReader();
-        MsgIntReader mir = new MsgIntReader();
-        ArrayList<IElementReader> readers = new ArrayList<>(2);
-        readers.add(mer);
-        readers.add(mlr);
-        readers.add(mir);
-        
-        Runner rnr = new Runner( readers );
-        
-        ArrayList<LineEntry> lines = rnr.run();
-        
+    	ArrayList<ElementReader> readers=new ArrayList<>(2);
+    	readers.add(new MsgLineReader(System.in));
+    	readers.add(new MsgElementReader(System.in));
+    	try
+    	{
+    		while(true)
+    		{
+    			for (var reader: readers)
+    			{
+    				lineRead=reader.readFromKeyboard();
+    				if(lineRead!=null)
+    				{
+    					if(lineRead.equalsIgnoreCase("QUIT"))
+    						break;
+    					TextBlock tb=new TextBlock(++lineNo,lineRead);
+    					lines.add(tb);
+    					System.out.println(lineRead);
+    				}else
+    					break;
+    			}
+    		}
+    	}
         lines.forEach(line ->
         {        
             System.out.println(line);
